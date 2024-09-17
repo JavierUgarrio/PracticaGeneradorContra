d37b9764f9bd0a37cff4fe76ee20112a51655ce6
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-17 11:37:11 +0200

    practica completada

--- GeneradorContrasenas/src/generadorcontrasenas/GeneradorContrasenas.java
+++ GeneradorContrasenas/src/generadorcontrasenas/GeneradorContrasenas.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package generadorcontrasenas;
 
 /**
@@ -15,6 +11,26 @@ public class GeneradorContrasenas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(genaradorContrasena());
+        
+    }
+    static String genaradorContrasena(){
+        char[] mayusculas ={'A','B','C','D','E','F','G','H','I','J'};
+        char[] minusculas ={'a','b','c','d','e','f','g','h','i','j'};
+        char[] numeros ={'0','1','2','3','4','5','6','7','8','9','0'};
+        StringBuilder  caracteres = new StringBuilder();
+        caracteres.append(mayusculas);
+        caracteres.append(minusculas);
+        caracteres.append(numeros);
+        
+        StringBuilder contrasena = new StringBuilder();
+        
+        for(int i =0; i<=15; i++){
+            int cantidadCaracteres = caracteres.length();
+            int numeroRandom = (int)(Math.random()*cantidadCaracteres);
+            contrasena.append((caracteres.toString()).charAt(numeroRandom));
+        }
+        return contrasena.toString();
     }
     
 }
